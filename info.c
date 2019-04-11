8a185a472c06495d4440648f78e7f1c54e183d81
Diego Gonzalez <dgonzl05@estudiantes.unileon.es>
2019-04-11 14:08:38 +0200

    Falta el filtro de nombres

--- info.c
+++ info.c
@@ -5,32 +5,59 @@
 #define MAX_CHAR 100
 #define MAX_LINEAS 5
 
-void leerMatriz();
-void filtro();
-void imprimirMatriz();
+void leerMatriz(char matriz[MAX_LINEAS][MAX_CHAR], FILE *file);
+void filtro(char matriz[MAX_LINEAS][MAX_CHAR], char letra);
 
 void leerMatriz(char matriz[MAX_LINEAS][MAX_CHAR], FILE *file){
 
+	int i = 0;
+	size_t bytes;
+	char *linea;
+	size_t tam;
 
+	do{
+		bytes=getline(&linea,&tam,file);
+		if(bytes!=-1){
+			strncpy(matriz[i],linea,bytes-1);
+			matriz[i][bytes-1]='\0';
+			i++;
+		}
+	}while(bytes!=-1);
 
+	free(linea);
+	fclose(file);
 }
 
-int main(int argc, char const *argv[3])
-{
+void filtro(char matriz[MAX_LINEAS][MAX_CHAR], char letra){
+
+	int i;
+	for (i = 0; i < 5; ++i)
+	{
+		
+		if (matriz[i][0] == letra);
+		{
+			printf("%s\n", matriz[i]);
+		}
+	}
+}
+
+int main(int argc, char const *argv[]){
+
 	FILE *file;
 
 	char matriz[MAX_LINEAS][MAX_CHAR];
+	char letra;
+	letra = *argv[2];
 
-	fopen(argv[1], "r");
+	file = fopen("info.txt", "r");
 
-	if (file == NULL)
-	{
+	if (file == NULL){
 		printf("El archivo no se ha podido abrir\n");
-		exit -1;
+		exit(-1);
 	}else{
 
 		leerMatriz(matriz, file);
-
+		filtro(matriz, letra);
 	}
 
 	return 0;
